77ec689ecb055ca6a08a1f3610365bda860c354e
KevinJ3259 <kmjordan2009.kj@gmail.com>
2022-08-06 06:31:37 -0500

    final commit with changes and readme screenshot

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # work-day-scheduler
+
 Table Of Contents:
+
 Description
+User Story
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+
+Acceptance Criteria
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+THEN I can enter an event
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+Screenshot
+![screenshot](https://i.imgur.com/zWPkbIK.png)
+
 Deployment
 Installation
 License
@@ -8,9 +33,9 @@ Description
 In this application, you will witness my use of ReactJS along with ReactHooks and more.
 
 Deployment:
-[GitHub] Repo: https://github.com/KevinJ3259/my-busy-schedule 
+[GitHub] Repo: https://github.com/KevinJ3259/my-busy-schedule
 
-GitHub Pages: https://kevinj3259.github.io/my-busy-schedule/git 
+GitHub Pages: https://kevinj3259.github.io/my-busy-schedule/git
 
 picture
 
@@ -28,5 +53,3 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- 
-
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,49 @@
-$(document).ready(function () {
-    // listen for save button clicks
-    $(".saveBtn").on("click", function () {
-        // get nearby values
-        let value = $(this).siblings(".description").val();
-        let time = $(this).parent().attr("id");
+$(document).ready(() => {
+  // listen for save button clicks
+  $(".saveBtn").on("click", function () {
+    // get nearby values
+    const value = $(this).siblings(".description").val();
+    const time = $(this).parent().attr("id");
 
-        // save in localStorage
-        localStorage.setItem(time, value);
+    // save in localStorage
+    localStorage.setItem(time, value);
+  });
+  const hourUpdater = () => {
+    const currentHour = moment().hour() + 5;
+    $(".time-block").each(function () {
+      const blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      if (blockHour < currentHour) {
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
     });
-    const hourUpdater = () => {
-        let currentHour = moment().hour();
-        $('.time-block').each(function() {
-            let blockHour = parseInt($(this).attr('id').split('-')[1]);
+  };
+  hourUpdater();
 
-            if (blockHour < currentHour) {
-                $(this).addClass('past');
-            }
-            else if (blockHour === currentHour) {
-                $(this).removeClass('past');
-                $(this).addClass('present');
-            }
-            else {
-                $(this).removeClass('past');
-                $(this).removeClass('present');
-                $(this).addClass('future');
-            }
-        })
-    }
-    hourUpdater();
+  // set up interval to check if current time needs to be updated
+  setInterval(hourUpdater, 15000);
 
-    // set up interval to check if current time needs to be updated
-    var interval = setInterval(hourUpdater, 15000);
+  // load any saved data from localStorage
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-    // load any saved data from localStorage
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-
-    // display current day on page
-    $("#currentDay").text(moment().format("dddd, MMMM Do"));
-
+  // display current day on page
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
